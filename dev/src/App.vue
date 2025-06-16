a32f5b2912d66f43c8bba64c555623f202fd921a
Milad Nazari <mnrmilad@gmail.com>
2025-06-16 14:17:13 +0330

    use check builded box

--- dev/src/App.vue
+++ dev/src/App.vue
@@ -1,16 +1,17 @@
 <script setup>
-import ViCheckBox from '@/components/ViCheckBox.vue';
+import { ViCheckBox } from '../../dist/index.es.js';
 import ViInput from '@/components/ViInput.vue';
 import { ref } from 'vue';
 
-const checkbox = ref(false);
+const checkbox = ref([]);
 </script>
 
 <template>
   <div class="bg-white mx-auto p-5 w-[800px]">
     <div class="p-2 grid gap-5">
       <div>
-        <ViCheckBox v-model="checkbox">click on me</ViCheckBox>
+        <ViCheckBox v-model="checkbox" value="ali">click on me</ViCheckBox>
+        {{ checkbox }}
       </div>
       <ViInput></ViInput>
     </div>
