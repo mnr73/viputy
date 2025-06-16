62eda49dc74da3fead72c35bdf930fff692fdd8c
Milad Nazari <mnrmilad@gmail.com>
2025-06-16 11:51:27 +0330

    add dev directory for develop components

---
+++ dev/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ViCheckBox from '@/components/ViCheckBox.vue'
+import ViInput from '@/components/ViInput.vue'
+
+// import ViCheckBox from '@lib/components/ViCheckBox.vue'
+// import ViCheckBox
+</script>
+
+<template>
+  <div class="bg-white mx-auto p-5 w-[800px]">
+    <div class="p-2 grid gap-5">
+      <ViCheckBox></ViCheckBox>
+      <ViInput></ViInput>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
